ec771abe6e32f939974078c5afc17a0f54aa09a8
CinyeeP <cinyeepoot@gmail.com>
2026-08-11 15:29:30 +1200

    Implemented slow down for moving, and added Amy/Princya's homing function.

--- src/motionController.cpp
+++ src/motionController.cpp
@@ -9,7 +9,9 @@
 // Homing base speeds
 int M2_speed = 100;
 int M1_speed = M2_speed * 1.3;
-
+#define MIN_SPEED 40
+#define MAX_SPEED 100
+#define SLOW_ZONE 500
 #define POSITION_TOLERANCE 10 // Tolerance in encoder counts for position control
 
 MotionController::MotionController(Encoder& encoder, float& absoluteX, float& absoluteY) : encoder(encoder), absoluteX(absoluteX), absoluteY(absoluteY) {
@@ -45,59 +47,98 @@ void MotionController::setTarget(float x, float y, float speed) {
     completed = false;
 }
 
-void MotionController::update() {
+void MotionController::update() { // This controls the motors
     Serial.println("=== update() ===");
 
     if (completed) {
         Serial.println("Motion already completed");
         return;
     }
-    long currentMotor1 = encoder.getMotor1Count();
-    long currentMotor2 = encoder.getMotor2Count();
+    long error1 = targetMotor1 - encoder.getMotor1Count();
+    long error2 = targetMotor2 - encoder.getMotor2Count();
 
     Serial.print("M1: ");
-    Serial.print(currentMotor1);
+    Serial.print(encoder.getMotor1Count());
     Serial.print(" / ");
     Serial.println(targetMotor1);
 
     Serial.print("M2: ");
-    Serial.print(currentMotor2);
+    Serial.print(encoder.getMotor2Count());
     Serial.print(" / ");
     Serial.println(targetMotor2);
 
+    int motor1Speed;
+    int motor2Speed;
+
     // MOTOR 1 CONTROL //
-    if (currentMotor1 < targetMotor1) {
-        Serial.println("M1: FORWARD");
-        // Move motor 1 forward
-        digitalWrite(motor1_pin, HIGH);
-        analogWrite(enable1_pin, speed);
-    } else if (currentMotor1 > targetMotor1) {
-        Serial.println("M1: BACKWARD");
-        // Move motor 1 backward
-        digitalWrite(motor1_pin, LOW);
-        analogWrite(enable1_pin, speed);
+    if (abs(error1) <= POSITION_TOLERANCE) {
+        motor1Speed = 0;
+    } else if (abs(error1) < SLOW_ZONE) {
+        motor1Speed = map(abs(error1), 0, SLOW_ZONE,
+                        MIN_SPEED, MAX_SPEED);
     } else {
-        Serial.println("M1: STOP");
-        // Stop motor 1
-        analogWrite(enable1_pin, 0);
+        motor1Speed = MAX_SPEED;
     }
+
+    if (error1 > 0) {
+        digitalWrite(motor1_pin, HIGH);
+    } else {
+        digitalWrite(motor1_pin, LOW);
+    }
+
+    analogWrite(enable1_pin, motor1Speed);
 
     // MOTOR 2 CONTROL //
-    if (currentMotor2 < targetMotor2) {
-        // Move motor 2 forward
-        digitalWrite(motor2_pin, HIGH);
-        analogWrite(enable2_pin, speed);
-    } else if (currentMotor2 > targetMotor2) {
-        // Move motor 2 backward
-        digitalWrite(motor2_pin, LOW);
-        analogWrite(enable2_pin, speed);
+    if (abs(error2) <= POSITION_TOLERANCE) {
+        motor2Speed = 0;
+    } else if (abs(error2) < SLOW_ZONE) {
+        motor2Speed = map(abs(error2), 0, SLOW_ZONE,
+                        MIN_SPEED, MAX_SPEED);
     } else {
-        // Stop motor 2
-        analogWrite(enable2_pin, 0);
+        motor2Speed = MAX_SPEED;
     }
 
+    if (error2 > 0) {
+        digitalWrite(motor2_pin, HIGH);
+    } else {
+        digitalWrite(motor2_pin, LOW);
+    }
+
+    analogWrite(enable2_pin, motor2Speed);
+
+    // // MOTOR 1 CONTROL //
+    // if (abs(error1) > POSITION_TOLERANCE) {
+    //     Serial.println("M1: FORWARD");
+    //     // Move motor 1 forward
+    //     digitalWrite(motor1_pin, HIGH);
+    //     analogWrite(enable1_pin, speed);
+    // } else if (abs(error1) > 0) {
+    //     Serial.println("M1: BACKWARD");
+    //     // Move motor 1 backward
+    //     digitalWrite(motor1_pin, LOW);
+    //     analogWrite(enable1_pin, speed);
+    // } else {
+    //     Serial.println("M1: STOP");
+    //     // Stop motor 1
+    //     analogWrite(enable1_pin, 0);
+    // }
+
+    // // MOTOR 2 CONTROL //
+    // if (abs(error2) > POSITION_TOLERANCE) {
+    //     // Move motor 2 forward
+    //     digitalWrite(motor2_pin, HIGH);
+    //     analogWrite(enable2_pin, speed);
+    // } else if (abs(error2) > 0) {
+    //     // Move motor 2 backward
+    //     digitalWrite(motor2_pin, LOW);
+    //     analogWrite(enable2_pin, speed);
+    // } else {
+    //     // Stop motor 2
+    //     analogWrite(enable2_pin, 0);
+    // }
+
     // Check if both motors have reached their targets //
-    if (currentMotor1 == targetMotor1 && currentMotor2 == targetMotor2) {
+    if (abs(error1) <= POSITION_TOLERANCE && abs(error2) <= POSITION_TOLERANCE) {
         Serial.println("=== TARGET REACHED ===");
         completed = true;
         // Stop both motors
@@ -147,13 +188,15 @@ void MotionController::Idle() {
 }
 
 void MotionController::Homing(const volatile SwitchState& switchState){
-    while((switchState == MotionController::START) || (switchState == MotionController::sT) || (switchState == MotionController::sB) || (switchState == MotionController::sR)){
-        if(switchState == MotionController::sB){
+    int a = 0;
+    while(switchState != MotionController::sL){
+        if(switchState == MotionController::sB && a == 0){
             digitalWrite(motor1_pin, LOW);
             digitalWrite(motor2_pin, HIGH);
             analogWrite(enable1_pin, M1_speed);
             analogWrite(enable2_pin, M2_speed); 
             delay(1000);
+            a = 1;
         }
         digitalWrite(motor1_pin, LOW);
         digitalWrite(motor2_pin, LOW);
@@ -162,22 +205,45 @@ void MotionController::Homing(const volatile SwitchState& switchState){
 
     }
     
-    Idle();
+    
+    analogWrite(enable1_pin, 0);
+    analogWrite(enable2_pin, 0);
+    delay(500);
+
     // implement logic to move to the right
     digitalWrite(motor1_pin, HIGH);
     digitalWrite(motor2_pin, HIGH);
     analogWrite(enable1_pin, M1_speed);
     analogWrite(enable2_pin, M2_speed);
     delay(500);
-    Idle();
+    
+    analogWrite(enable1_pin, 0);
+    analogWrite(enable2_pin, 0);
+    delay(500);
 
-    while((switchState == MotionController::START) || (switchState == MotionController::sT) || (switchState == MotionController::sL) || (switchState == MotionController::sR)){
+    // while((switchState == MotionController::START) || (switchState == MotionController::sT) || (switchState == MotionController::sL) || (switchState == MotionController::sR)){
+    while(switchState != MotionController::sB){
         digitalWrite(motor1_pin, HIGH);
         digitalWrite(motor2_pin, LOW);
         analogWrite(enable1_pin, M1_speed);
         analogWrite(enable2_pin, M2_speed);
+
     }
-    Idle();
+    
+    analogWrite(enable1_pin, 0);
+    analogWrite(enable2_pin, 0);
+    delay(500);
+    
+    digitalWrite(motor1_pin, LOW);
+    digitalWrite(motor2_pin, HIGH);
+    analogWrite(enable1_pin, M1_speed);
+    analogWrite(enable2_pin, M2_speed);
+    delay(500);
+    
+    analogWrite(enable1_pin, 0);
+    analogWrite(enable2_pin, 0);
+    delay(500);
+
     absoluteX = 0.0f;
     absoluteY = 0.0f;
     encoder.resetCounts();
