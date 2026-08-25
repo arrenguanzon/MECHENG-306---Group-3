bb8721e3375f528f5fe628955261ade4d7ac45bb
arrenguanzon <arren@LAPTOP-D7COVUUS.localdomain>
2026-08-26 11:57:00 +1200

    FEEDFORWARD CONTROL - TUNING REQUIRED

--- src/motionController.cpp
+++ src/motionController.cpp
@@ -15,8 +15,9 @@ int MAX_SPEED = 100; // tune --> could be larger
 float ACCELERATION_RATE = 0.04f; // PWM units per millisecond (e.g., 0.04 PWM/ms = 0.75s to go from 70 PWM to 100 PWM)
 
 // Controller gains (kp + ki > 0.05 for error = 100mm [~3000 encoder counts] to output min. speed of 75)
-float Kp = 0.05f; // tune
-float Ki = 0.00f; // tune
+float Kp = 0.00f; // TUNE FIRST: eliminate error
+float Ki = 0.00f; // tune second: eliminate steady state error
+float Kv = 0.7f; // Kv = 0.7 + Kp = *value* should be enough to correct position errors
 
 // Position tolerance (acceptable error in encoder counts)
 int positionTolerance = 800; //adjust based on testing
@@ -49,14 +50,14 @@ void MotionController::setTarget(float x, float y, float speed) {
 
     calculateMotorTargets();
 
-    // PRINTING /////////////////////////////////////////////////////////////////
-    Serial.println("TARGET MOTOR COUNTS [relative to current position when called]");
-    Serial.print("Target Motor 1 Counts: ");
-    Serial.println(targetMotor1);
-    Serial.print("Target Motor 2 Counts: ");
-    Serial.println(targetMotor2);
-    Serial.println(" ");
-    /////////////////////////////////////////////////////////////////////////////
+    // // PRINTING /////////////////////////////////////////////////////////////////
+    // Serial.println("TARGET MOTOR COUNTS [relative to current position when called]");
+    // Serial.print("Target Motor 1 Counts: ");
+    // Serial.println(targetMotor1);
+    // Serial.print("Target Motor 2 Counts: ");
+    // Serial.println(targetMotor2);
+    // Serial.println(" ");
+    // /////////////////////////////////////////////////////////////////////////////
     
     completed = false;
 }
@@ -80,27 +81,27 @@ void MotionController::update() {
     long currentMotor1 = encoder.getMotor1Count();
     long currentMotor2 = encoder.getMotor2Count();
 
-    ////////////////////////////////////////////////////////
-    Serial.println("CURRENT MOTOR POSITION [relative to position when command was called; increases from 0 to target]");
-    Serial.print("Current motor 1: ");
-    Serial.println(currentMotor1);
-    Serial.print("Current motor 2: ");
-    Serial.println(currentMotor2);
-    Serial.println("  ");
-    ////////////////////////////////////////////////////////
+    // ////////////////////////////////////////////////////////
+    // Serial.println("CURRENT MOTOR POSITION [relative to position when command was called; increases from 0 to target]");
+    // Serial.print("Current motor 1: ");
+    // Serial.println(currentMotor1);
+    // Serial.print("Current motor 2: ");
+    // Serial.println(currentMotor2);
+    // Serial.println("  ");
+    // ////////////////////////////////////////////////////////
 
     // Calculate errors (direction of motion is determined by the sign of the error)
     long motor1Error = targetMotor1 - currentMotor1;
     long motor2Error = targetMotor2 - currentMotor2;
 
-    ////////////////////////////////////////////////////////
-    Serial.println("MOTOR ERRORS [should decrease to 100]"); // (decreases until 100 [position tolerance])
-    Serial.print("Motor 1 error: ");
-    Serial.println(motor1Error);
-    Serial.print("Motor 2 error: ");
-    Serial.println(motor2Error);
-    Serial.println("  ");
-    ////////////////////////////////////////////////////////
+    // ////////////////////////////////////////////////////////
+    // Serial.println("MOTOR ERRORS [should decrease to 100]"); // (decreases until 100 [position tolerance])
+    // Serial.print("Motor 1 error: ");
+    // Serial.println(motor1Error);
+    // Serial.print("Motor 2 error: ");
+    // Serial.println(motor2Error);
+    // Serial.println("  ");
+    // ////////////////////////////////////////////////////////
 
     // MOTOR 1
     if (abs(motor1Error) <= positionTolerance)  {
@@ -114,7 +115,7 @@ void MotionController::update() {
         // Integral terms for PID control
         integralMotor1 = prevIntegralMotor1 + Ki * motor1Error;
         // Motor output
-        float speedMotor1 = abs(Kp * (float)motor1Error + integralMotor1); // must be positive for analogWrite
+        float speedMotor1 = abs(Kp * (float)motor1Error + integralMotor1 + Kv * currentRampedMaxSpeed); // must be positive for analogWrite
 
         // Integral clamping to prevent windup
         if (speedMotor1 > currentRampedMaxSpeed) {
@@ -130,11 +131,11 @@ void MotionController::update() {
             speedMotor1 = MIN_SPEED;
         }
 
-        ////////////////////////////////////////////////////////
-        Serial.println("MOTOR 1 SPEED [max of 100; should decrease the end]"); 
-        Serial.print("Motor 1 speed: ");
-        Serial.println(speedMotor1);
-        ////////////////////////////////////////////////////////
+        // ////////////////////////////////////////////////////////
+        // Serial.println("MOTOR 1 SPEED [max of 100; should decrease the end]"); 
+        // Serial.print("Motor 1 speed: ");
+        // Serial.println(speedMotor1);
+        // ////////////////////////////////////////////////////////
 
         // MOTOR 1 CONTROL //
         digitalWrite(motor1_pin, motor1Error > 0 ? HIGH : LOW);
@@ -154,7 +155,7 @@ void MotionController::update() {
         integralMotor2 = prevIntegralMotor2 + Ki * motor2Error;
 
         // Calculate motor speed outputs
-        float speedMotor2 = abs(Kp * (float)motor2Error + integralMotor2);
+        float speedMotor2 = abs(Kp * (float)motor2Error + integralMotor2 + Kv * currentRampedMaxSpeed); // must be positive for analogWrite
 
         // Integral clamping to prevent windup
         if (speedMotor2 > currentRampedMaxSpeed) {
@@ -169,12 +170,12 @@ void MotionController::update() {
             speedMotor2 = MIN_SPEED;
         }
 
-        ////////////////////////////////////////////////////////
-        Serial.println("MOTOR 2 SPEED [max of 100; should decrease the end]"); 
-        Serial.print("Motor 2 speed: ");
-        Serial.println(speedMotor2);
-        Serial.println(" ");
-        ////////////////////////////////////////////////////////
+        // ////////////////////////////////////////////////////////
+        // Serial.println("MOTOR 2 SPEED [max of 100; should decrease the end]"); 
+        // Serial.print("Motor 2 speed: ");
+        // Serial.println(speedMotor2);
+        // Serial.println(" ");
+        // ////////////////////////////////////////////////////////
 
         // MOTOR 2 CONTROL //
         digitalWrite(motor2_pin, motor2Error > 0 ? HIGH : LOW);
